afad1a930ac22faab4850ae01de2f28c030b088f
maxpaz <maxpaz2004@gmail.com>
2015-12-03 17:59:44 +0000

    HW4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -101,19 +101,19 @@ RSpec.configure do |config|
   config.include DoubleAliases
 end
 
-RSpec.configure do |config|
+#RSpec.configure do |config|
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
+#  config.before(:suite) do
+#    DatabaseCleaner.strategy = :transaction
+#    DatabaseCleaner.clean_with(:truncation)
+#  end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+#  config.before(:each) do
+#    DatabaseCleaner.start
+#  end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+#  config.after(:each) do
+#    DatabaseCleaner.clean
+#  end
 
-end
+#end
