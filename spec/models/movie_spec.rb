db6939e03cd60c6906695b11e9f47c33d5b79780
maxpaz <maxpaz2004@gmail.com>
2015-12-04 01:38:06 +0000

    HW4

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,13 +3,13 @@ describe Movie do
     before :each do
       Movie.create(title: 'Star Wars', rating: 'PG', director: "George Lucas", release_date: '1977-05-25')
       Movie.create(title: 'Blade Runne', rating: 'PG', director: "Ridley Scott", release_date: '1982-06-25')
-      @movie = FactoryGirl.build(:movie,:id => 1, :title=> 'Star Wars', :rating => 'PG', :director => 'George Lucas', :release_date => '1977-05-25', :created_at => '2015-11-28 14:48:56', :updated_at => '2015-11-28 14:48:56')
+      #@movie = FactoryGirl.build(:movie,:id => 1, :title=> 'Star Wars', :rating => 'PG', :director => 'George Lucas', :release_date => '1977-05-25', :created_at => '2015-11-28 14:48:56', :updated_at => '2015-11-28 14:48:56')
     end  
     it 'should find movies by same director' do
       #Tmdb::Movie.should_receive(:find).with('Inception')
       
       Movie.director_related_movies(1).each do |movie|
-        movie.director.should == @movie.director
+        movie.director.should =="Ridley Scott"
       end
     end
     it 'should not find movies by different directors' do
